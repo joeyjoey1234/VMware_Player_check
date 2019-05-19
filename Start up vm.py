034464fae4a0a61441d4f543c894726fc4f6498e
joeyjoey1234 <37442571+joeyjoey1234@users.noreply.github.com>
2019-05-19 10:11:44 -0600

    Add files via upload

--- Start up vm.py
+++ Start up vm.py
@@ -3,6 +3,6 @@ import subprocess
 
 
 
-subprocess.call("'C:\Program Files (x86)\VMware\VMware Player\vmplayer.exe' 'E:\ssh server\SSH server.vmx'")
+subprocess.call('C:\Program Files (x86)\VMware\VMware Player\\vmplayer.exe')
 
 
